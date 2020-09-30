00e2e7d67d956d68dc2d2cda26a89fbff1347728
GitHub Action <action@github.com>
2020-09-30 09:39:32 +0000

    Update documentation skip-checks: true

--- navtreedata.js
+++ navtreedata.js
@@ -2,6 +2,7 @@ var NAVTREE =
 [
   [ "alpaka", "index.html", [
     [ "**alpaka** - Abstraction Library for Parallel Kernel Acceleration", "index.html", null ],
+    [ "Todo List", "todo.html", null ],
     [ "Namespaces", null, [
       [ "Namespace List", "namespaces.html", "namespaces" ],
       [ "Namespace Members", "namespacemembers.html", [
@@ -40,20 +41,21 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "AbsStdLib_8hpp.html",
-"ForEachType_8hpp.html#a84fa9b71d57092b3d9da78ee5eea68b4",
-"WorkDivHelpers_8hpp.html#aba5c202eb52a626944b0d52323537289",
-"classalpaka_1_1core_1_1detail_1_1ITaskPkg.html#a60e339b89dc58f8a79669ed3458cff9c",
-"classalpaka_1_1math_1_1CbrtUniformCudaHipBuiltIn.html",
-"classalpaka_1_1vec_1_1Vec.html#a2722008fb0eb8b78d8f3bc8e207989df",
-"math_2asin_2Traits_8hpp.html",
-"namespacealpaka_1_1mem_1_1view_1_1traits_1_1detail.html",
-"structalpaka_1_1atomic_1_1op_1_1Xor.html#ac61a364f15e0206a077904e26daf82a4",
-"structalpaka_1_1dev_1_1traits_1_1DevType_3_01acc_1_1AccCpuSerial_3_01TDim_00_01TIdx_01_4_01_4.html#a9d7721a79ac59a541b8cbfcafce8af62",
-"structalpaka_1_1idx_1_1traits_1_1GetIdx_3_01idx_1_1bt_1_1IdxBtUniformCudaHipBuiltIn_3_01TDim_00_7955b12235245a78444a51912fba9274.html",
-"structalpaka_1_1math_1_1traits_1_1Max_3_01MaxUniformCudaHipBuiltIn_00_01Tx_00_01Ty_00_01std_1_1eb9e2652c8be34e74fa79706d46758abf.html",
+"Filter_8hpp.html#a65e851602fef62781ed5eb23fb1d8fd6",
+"Vec_8hpp.html#ac35e2d3485ff8618460af02ce92a71e1",
+"classalpaka_1_1block_1_1st_1_1BlockSharedMemStUniformCudaHipBuiltIn.html",
+"classalpaka_1_1intrinsic_1_1IntrinsicFallback.html#a55793cc12b425988725a17eb4cdcafd2",
+"classalpaka_1_1queue_1_1generic_1_1detail_1_1QueueGenericThreadsNonBlockingImpl.html#a7b0ca0bb827f32ba8c59ec35eca51414",
+"extent_2Traits_8hpp.html#aa15d83e423b1c298fc62193686c4737a",
+"namespacealpaka_1_1core_1_1align.html",
+"queue_2Traits_8hpp.html#add43e076d64ca901491be83f21cb6ef3",
+"structalpaka_1_1block_1_1op_1_1LogicalAnd.html#af1b671339892858b1db50019acf3332bae2d44d903f2c64859a72b4737c2d58a0",
+"structalpaka_1_1dim_1_1traits_1_1DimType_3_01T_00_01std_1_1enable__if__t_3_01std_1_1is__same_3_097b7e6ee7b3274fad3af261f7afe04a5.html",
+"structalpaka_1_1intrinsic_1_1ConceptIntrinsic.html",
+"structalpaka_1_1math_1_1traits_1_1Trunc_3_01TruncUniformCudaHipBuiltIn_00_01TArg_00_01std_1_1enaab84821241abd21bc8d0f2710f16f996.html",
 "structalpaka_1_1mem_1_1view_1_1uniform__cuda__hip_1_1detail_1_1TaskCopyUniformCudaHip_3_01dim_1_2efdf378c7cab3dc2b6b093e79813a5f.html#a1eb70d14edc76743c04756059c5e94f6",
-"structalpaka_1_1queue_1_1traits_1_1Enqueue_3_01queue_1_1QueueUniformCudaHipRtBlocking_00_01mem_1e3ef7610d8b77693579148eb8b04ec49.html#a1ef009c15b63401c15f1e06cc609dd1c",
-"version_8hpp_source.html"
+"structalpaka_1_1queue_1_1traits_1_1Enqueue_3_01queue_1_1QueueUniformCudaHipRtBlocking_00_01kerne2026d428c3379e41ad16abfd0c583de6.html#a9c1bc1034dbb73753cab75f5a7d20f3b",
+"uniformCudaHip_2Copy_8hpp.html#a6a5e53d5b071b1c14a7880a890193c4c"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
