a22951f0b4d4f5a2650e59c4087e0830d63471c5
GitHub Action <action@github.com>
2021-01-13 13:17:03 +0000

    Update documentation skip-checks: true

--- search/typedefs_11.js
+++ search/typedefs_11.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['val',['Val',['../classalpaka_1_1Vec.html#a8ba1d21778eba24f0ada0ec057204c8b',1,'alpaka::Vec']]],
-  ['vec2d',['vec2D',['../namespacealpaka_1_1detail.html#a647b05ec1646c5c8ff79c14eaedf8336',1,'alpaka::detail']]],
-  ['vec3d',['vec3D',['../namespacealpaka_1_1detail.html#a18dd2696879f8bb24959465faa1327fc',1,'alpaka::detail']]]
+  ['void',['Void',['../namespacealpaka_1_1meta.html#a74d93e6a2efc8f3ac7573990155bdf2c',1,'alpaka::meta']]]
 ];
